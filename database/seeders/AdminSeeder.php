2f2a9e0d1e72b9662a3374d2b8106a57e2d4e5ba
Abdulkader-HM <Aboude-hm7@hotmail.com>
2022-07-30 01:21:45 +0200

    admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+
+            ['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => '$2y$10$m7ZVDmTVf2a1hrJ9R/D4k.l3ZYGwL8BXCloJa/EaEFRcgeKOzc0K6', 'is_admin' => '1', 'email_verified_at' => '2022-07-28 22:54:55']
+        ];
+        User::insert($admin);
+    }
+}
